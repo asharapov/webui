d41039dd60740454b3204f84da402555709c3e96
Anton Sharapov <asharapov2@yandex.ru>
2010-06-15 02:08:53 +0400

    Added model for .wui tag handlers

--- java/org/echosoft/framework/ui/core/xml/TagLibrary.java
+++ java/org/echosoft/framework/ui/core/xml/TagLibrary.java
@@ -1,6 +1,11 @@
 package org.echosoft.framework.ui.core.xml;
 
-import org.echosoft.common.model.Version;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.echosoft.common.utils.ObjectUtil;
+import org.echosoft.common.utils.StringUtil;
 
 /**
  * Описывает библиотеку тегов - элементов описания web страниц на xml.  
@@ -8,7 +13,43 @@ import org.echosoft.common.model.Version;
  */
 public class TagLibrary {
 
-    private String uri;
-    private Version version;
+    private final String uri;
+    private final Map<String,TagHandler> tags;
 
+    public TagLibrary(final String uri) {
+        this.uri = StringUtil.trim(uri);
+        this.tags = new HashMap<String,TagHandler>();
+    }
+
+    /**
+     * Возвращает уникальный идентификатор библиотеки тегов (включая версию).
+     * @return  уникальный идентификатор данной библиотеки тегов.
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * Регистрирует в библиотеке новый тег.
+     * @param tagName  имя тега (чуствительно к регистру).
+     * @param className  имя класса - обработчика. Должен реализовывать интерфейс {@link TagHandler}.
+     * @throws InvocationTargetException  поднимается в случае ошибки конструирования экземпляра класса {@link TagHandler} по имени его класса.
+     * @throws ClassNotFoundException  поднимается в случае ошибки конструирования экземпляра класса {@link TagHandler} по имени его класса.
+     * @throws NoSuchMethodException  поднимается в случае ошибки конструирования экземпляра класса {@link TagHandler} по имени его класса.
+     * @throws IllegalAccessException  поднимается в случае ошибки конструирования экземпляра класса {@link TagHandler} по имени его класса.
+     * @throws InstantiationException  поднимается в случае ошибки конструирования экземпляра класса {@link TagHandler} по имени его класса.
+     */
+    public void addTagHandler(final String tagName, final String className) throws InvocationTargetException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException {
+        final TagHandler handler = ObjectUtil.makeInstance(className, TagHandler.class);
+        tags.put(tagName, handler);
+    }
+
+    /**
+     * Возвращает обработчик тега по его имени.
+     * @param tagName  имя тега (чуствительно к регистру).
+     * @return  экземпляр класса {@link TagHandler} или <code>null</code> если указанный тег не зарегистрирован в библиотеке.
+     */
+    public TagHandler getTagHandler(final String tagName) {
+        return tags.get(tagName);
+    }
 }
---
+++ java/org/echosoft/framework/ui/core/xml/TagLibrarySet.java
@@ -0,0 +1,54 @@
+package org.echosoft.framework.ui.core.xml;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Содержит ссылки на все загруженные библиотеки тегов.
+ * @author Anton Sharapov
+ */
+public class TagLibrarySet {
+
+    private final Map<String,TagLibrary> libraries;
+
+    public TagLibrarySet() {
+        this.libraries = new HashMap<String,TagLibrary>();
+    }
+
+    /**
+     * Осуществляет поиск библиотеки тегов в списке уже зарегистрированых библиотек.
+     * Если таковая в списке еще не значится то регистрирует ее.
+     * @param uri  URI используемый в качестве уникального идентификатора библиотеки тегов.
+     * @return  описание указаной библиотеки. Никогда не возвращает <code>null</code>.
+     */
+    public TagLibrary ensureLibraryExists(final String uri) {
+        TagLibrary library = libraries.get(uri);
+        if (library==null) {
+            library = new TagLibrary(uri);
+            libraries.put(uri, library);
+        }
+        return library;
+    }
+
+    /**
+     * Осуществляет поиск библиотеки тегов в списке уже зарегистрированых библиотек.
+     * Если таковая в списке не значится то метод возвращает <code>null</code>.
+     * @param uri  URI используемый в качестве уникального идентификатора библиотеки тегов.
+     * @return  описание указаной библиотеки или <code>null</code> если таковая еще не зарегистрирована.
+     */
+    public TagLibrary getLibrary(final String uri) {
+        return libraries.get(uri);
+    }
+
+    /**
+     * В библиотеке тегов обозначенной под соответствующем URI ищется обработчик указанного в аргументе тега.
+     * @param uri  URI используемый в качестве уникального идентификатора библиотеки тегов.
+     * @param tagName  локальное имя тега.
+     * @return  обработчик указанного тега или <code>null</code> в случае отсутствия информации по указанной библиотеке или отсутствия в этой
+     * библиотеке информации по указанному тегу.
+     */
+    public TagHandler getTagHandler(final String uri, final String tagName) {
+        final TagLibrary library = libraries.get(uri);
+        return library!=null ? library.getTagHandler(tagName) : null;
+    }
+}
